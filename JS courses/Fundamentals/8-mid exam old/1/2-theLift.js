97ccadd6ac3c07208abf50b4e4e081144ecbada0
GeorgiGotev <g.gotev@abv.bg>
2023-01-28 02:54:06 +0200

    old exams

--- JS courses/Fundamentals/8-mid exam old/1/2-theLift.js
+++ JS courses/Fundamentals/8-mid exam old/1/2-theLift.js
@@ -2,33 +2,39 @@ function theLift(arr) {
     let firstEl = arr.shift();
     let people = Number(firstEl);
     let wagons = arr[0].split(' ').map(Number);
-    let newWagons = []
-    for (let i = 0; i < wagons.length; i++) {
-        let currWagon = wagons[i];
-        for (let i = 1; i <= 4; i++) {
-            if (people === 0) {
-                newWagons.push(currWagon);
-                break;
-            }
-            people--;
-            currWagon += 1;
-            if (currWagon === 4) {
-                newWagons.push(currWagon);
-                break;
-            }
-        }
+    let flag = false;
 
+    for (let i = 0; i < wagons.length; i++) {
+
+        let currWagon = wagons[i];
+        if (currWagon < 4) {
+            if (people >= 4 - currWagon) {
+                people = people - (4 - currWagon);
+                wagons[i] = 4
+            } else {
+                wagons[i] = people + currWagon;
+                people = 0;
+                if (wagons[i] < 4) {
+                    flag = true;
+                }
+            }
+        } else {
+            wagons[i] = 4;
+        }
     }
-    if ((newWagons[newWagons.length - 1]) < 4) {
+
+    if (flag) {
         console.log(`The lift has empty spots!`);
     }
+
     if (people > 0) {
         console.log(`There isn't enough space! ${people} people in a queue!`);
     }
-    console.log(newWagons.join(' '));
+    console.log(wagons.join(' '));
 
 }
 theLift([
-    "3",
-    "0 2 0"
+    "5",
+    "0 2 0 0"
 ]);
+
