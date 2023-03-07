63ac0fab90d5b81297e1fcfcb31de24e71867299
GeorgiGotev <g.gotev@abv.bg>
2023-03-07 23:36:25 +0200

    old final exams

--- JS courses/Fundamentals/final exam old/2/1-worldTour.js
+++ JS courses/Fundamentals/final exam old/2/1-worldTour.js
@@ -1,9 +1,15 @@
-function worldTour(input){
-    
+function worldTour(input) {
+    let line = input.shift();
+
+    while (line !== 'Travel') {
+        console.log(line);
+
+        line=input.shift();
+    }
 }
 worldTour((["Hawai::Cyprys-Greece",
-"Add Stop:7:Rome",
-"Remove Stop:11:16",
-"Switch:Hawai:Bulgaria",
-"Travel"])
+    "Add Stop:7:Rome",
+    "Remove Stop:11:16",
+    "Switch:Hawai:Bulgaria",
+    "Travel"])
 );
