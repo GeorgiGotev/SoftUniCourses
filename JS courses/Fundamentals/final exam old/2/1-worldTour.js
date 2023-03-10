3403aa53c172a555fde36eec8dcc923121114305
GeorgiGotev <g.gotev@abv.bg>
2023-03-10 15:13:02 +0200

    old final exams

--- JS courses/Fundamentals/final exam old/2/1-worldTour.js
+++ JS courses/Fundamentals/final exam old/2/1-worldTour.js
@@ -1,11 +1,36 @@
 function worldTour(input) {
+    let resultString = input.shift();
     let line = input.shift();
 
     while (line !== 'Travel') {
-        console.log(line);
+        let [command, index1, index2] = line.split(':');
+        switch (command) {
+            case 'Add Stop':
+                index1 = Number(index1);
+                if (index1 >= 0 && index1 < resultString.length) {
+                    resultString = resultString.substring(0, index1) + index2 + resultString.substring(index1);
+                }
+                console.log(resultString);
+                break;
+            case 'Remove Stop':
+                index1 = Number(index1);
+                index2 = Number(index2);
+                if (index1 >= 0 && index1 < resultString.length && index2 >= 0 && index2 < resultString.length) {
+                    resultString = resultString.slice(0, index1) + resultString.slice(index2 + 1);
+                }
+                console.log(resultString);
+                break;
+            case 'Switch':
+                if (resultString.includes(index1)) {
+                    resultString = resultString.replace(index1, index2);
+                }
+                console.log(resultString);
+                break;
+        }
 
-        line=input.shift();
+        line = input.shift();
     }
+    console.log(`Ready for world tour! Planned stops: ${resultString}`);
 }
 worldTour((["Hawai::Cyprys-Greece",
     "Add Stop:7:Rome",
