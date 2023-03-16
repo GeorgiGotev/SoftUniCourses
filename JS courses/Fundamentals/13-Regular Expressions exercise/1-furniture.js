7f60667d2b87db3704f4e8a7cbb6d4703ea7e550
Georgi Gotev <118106429+GeorgiGotev@users.noreply.github.com>
2023-03-16 14:49:44 +0200

    regex exercise solves

--- JS courses/Fundamentals/13-Regular Expressions exercise/1-furniture.js
+++ JS courses/Fundamentals/13-Regular Expressions exercise/1-furniture.js
@@ -1,4 +1,24 @@
-function furniture(){
+function furniture(arr) {
+    let pattern=/>>(?<name>[A-Za-z+]+)<<(?<price>\d+\.*\d*)!(?<count>\d+)/g;
+    let totalPrice=0;
+    let line=arr.shift();
+    console.log('Bought furniture:');
+    while(line!=='Purchase'){
+        
+       let product = pattern.exec(line);
+       while(product!== null){
+        console.log(product.groups.name);
+        totalPrice+=Number(product.groups.price) * Number(product.groups.count)
+        product = pattern.exec(line);
+    }
+
+        line=arr.shift();
+    }
+    console.log(`Total money spend: ${totalPrice.toFixed(2)}`);
 
 }
-furniture();
+furniture(['>>Sofa<<312.23!3',
+    '>>TV<<300!5',
+    '>Invalid<<!5',
+    'Purchase']
+);
