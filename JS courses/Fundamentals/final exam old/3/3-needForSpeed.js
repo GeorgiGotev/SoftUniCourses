e7de9de40379ca72a26f3766e9ac6b10aea28726
Georgi Gotev <118106429+GeorgiGotev@users.noreply.github.com>
2023-03-17 21:01:03 +0200

    old final exams

--- JS courses/Fundamentals/final exam old/3/3-needForSpeed.js
+++ JS courses/Fundamentals/final exam old/3/3-needForSpeed.js
@@ -1,5 +1,49 @@
 function needForSpeed(data) {
-    
+    let numberOfCars = data.shift();
+    let obj = {};
+    for (let i = 0; i < numberOfCars; i++) {
+        let [car, km, fuel] = data.shift().split('|');
+        obj[car] = {
+            km: Number(km),
+            fuel: Number(fuel),
+        }
+    }
+    let line = data.shift();
+    while (line !== 'Stop') {
+        let [command, car, cm1, cm2] = line.split(' : ');
+        cm1 = Number(cm1);
+        cm2 = Number(cm2);
+        switch (command) {
+            case 'Drive':
+                if (obj[car].fuel < cm2) {
+                    console.log('Not enough fuel to make that ride');
+                } else {
+                    obj[car].km += cm1;
+                    obj[car].fuel -= cm2;
+                    console.log(`${car} driven for ${cm1} kilometers. ${cm2} liters of fuel consumed."`);
+                }
+                if (obj[car].km >= 100000) {
+                    console.log(`Time to sell the ${obj[car]}!`);
+                }
+                break;
+            case 'Refuel':
+                let ref = 0;
+                if ((cm1 + obj[car].fuel )> 75) {
+                    ref = 75 - obj[car].fuel;
+                    obj[car].fuel = 75;
+                } else {
+                    obj[car].fuel += cm1;
+                    ref = cm1;
+                }
+                console.log(`${car} refueled with ${ref} liters`);
+                break;
+            case 'Revert':
+
+                break;
+        }
+        line = data.shift();
+    }
+
 }
 needForSpeed([
     '3',
