936ea40a9b14b9c736f647cf6044e72e75ae77a8
Georgi Gotev <118106429+GeorgiGotev@users.noreply.github.com>
2023-04-25 23:32:20 +0300

    js advanced solves

--- JS courses/Advanced/JS Advanced/4-1 Exercise DOM Introduction/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/06. Format the Text/solution.js
+++ JS courses/Advanced/JS Advanced/4-1 Exercise DOM Introduction/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/06. Format the Text/solution.js
@@ -1,26 +1,25 @@
 function solve() {
-  let inputElement = document.getElementById('input').value;
+  let inputElement = document.getElementById('input').value.trim();
   let output = document.getElementById('output');
-
+  output.innerHTML = '';
   let inputArr = inputElement.split('.').filter((p) => p.length > 0);
 
-  // for (let i = 0; i < input.length; i += 3) {
+  // for (let i = 0; i < inputArr.length; i += 3) {   
   //   let arr = [];
-   
   //   for (let y = 0; y < 3; y++) {
-  //     if (input[i + y]) {
-  //       arr.push(input[i + y]);
+  //     if (inputArr[i + y]) {
+  //       arr.push(inputArr[i + y]);
   //     }
   //   }
-  //   let result = arr.join('. ') + '.';
+  //   let result = arr.join('.') + '.';
     
   //   output.innerHTML += `<p>${result}</p>`;
   // }
 
   while(inputArr.length>0){  // better!!!!!!!
-    let text = inputArr.splice(0,3).map(x=>x + '.'); // or down one;
+    let text = inputArr.splice(0,3); // .map(x=>x + '.'); // or down one;
     let p=document.createElement('p');
-    p.textContent=text;    // or .join('. ') + '.';
+    p.textContent=text.join('. ') + '.'.trim();    // or .join('. ') + '.';
     output.appendChild(p);
   }
 
