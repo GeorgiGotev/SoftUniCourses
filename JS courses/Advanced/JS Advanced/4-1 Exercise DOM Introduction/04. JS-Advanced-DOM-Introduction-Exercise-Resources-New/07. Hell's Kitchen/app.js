4098c35c3b6f8963a6714b65c2dff46f5ff11f19
Georgi Gotev <118106429+GeorgiGotev@users.noreply.github.com>
2023-04-27 21:54:47 +0300

    hells kitchen dom

--- JS courses/Advanced/JS Advanced/4-1 Exercise DOM Introduction/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/07. Hell's Kitchen/app.js
+++ JS courses/Advanced/JS Advanced/4-1 Exercise DOM Introduction/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/07. Hell's Kitchen/app.js
@@ -1,8 +1,54 @@
 function solve() {
    document.querySelector('#btnSend').addEventListener('click', onClick);
+   let result = [];
+   function onClick() {
+      let inputArea = document.querySelector('#inputs textarea');
+      let bestRest = document.querySelector('#bestRestaurant p');
+      let bestRestWorkers = document.querySelector('#workers p');
 
-   function onClick () {
-      //   TODO:
-      
+      let obj = JSON.parse(inputArea.value);
+
+
+      for (const line of obj) {
+         let [name, workersData] = line.split(' - ');
+         let workersInfo = workersData && workersData.split(', ');
+
+         if (!result.find(r => r.name === name)) {
+            result.push({
+               name,
+               sumSalary: 0,
+               avgSalary: 0,
+               bestSalary: 0,
+               workers: [],
+            });
+         }
+         let currRest = result.find(e => e.name === name);
+         for (const worker of workersInfo) {
+            let [workerName, workerSalary] = worker.split(' ');
+            workerSalary = Number(workerSalary);
+
+            currRest.sumSalary += workerSalary;
+
+            if (currRest.bestSalary < workerSalary) {
+               currRest.bestSalary = workerSalary;
+            }
+            currRest.workers.push({
+               workerName,
+               workerSalary,
+            })
+         }
+         currRest.avgSalary = (currRest.sumSalary / currRest.workers.length).toFixed(2);
+      }
+
+      let bestRestName = result.sort((a, b) => b.avgSalary - a.avgSalary)[0];
+      let workersSorted = bestRestName.workers.sort((a, b) => b.workerSalary - a.workerSalary);
+      let buf = '';
+      bestRest.textContent = `Name: ${bestRestName.name} Average Salary: ${bestRestName.avgSalary} Best Salary: ${bestRestName.workers[0].workerSalary.toFixed(2)}`;
+      for (const worker of bestRestName.workers) {
+         buf += `Name: ${worker.workerName} With Salary: ${worker.workerSalary} `;
+      }
+      bestRestWorkers.textContent = buf;
    }
 }
+
+
