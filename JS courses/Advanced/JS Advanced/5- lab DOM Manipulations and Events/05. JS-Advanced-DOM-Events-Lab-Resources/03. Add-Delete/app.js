d2ec64c89469699b226da1c171b98ddf90c13005
Georgi Gotev <118106429+GeorgiGotev@users.noreply.github.com>
2023-05-02 23:43:33 +0300

    advanced solves

--- JS courses/Advanced/JS Advanced/5- lab DOM Manipulations and Events/05. JS-Advanced-DOM-Events-Lab-Resources/03. Add-Delete/app.js
+++ JS courses/Advanced/JS Advanced/5- lab DOM Manipulations and Events/05. JS-Advanced-DOM-Events-Lab-Resources/03. Add-Delete/app.js
@@ -26,9 +26,11 @@ function addItem() {
     itemForDelete.textContent = '[Delete]';
     liElement.appendChild(itemForDelete);
 
-    itemForDelete.addEventListener('click', (e) => {
-        e.currentTarget.parentElement.remove();
-    });
+    // itemForDelete.addEventListener('click', (e) => {
+    //     e.currentTarget.parentElement.remove();
+    // });
+
+    itemForDelete.addEventListener('click', ()=> liElement.remove()); // second variation of remove
 
     inputElement.value = '';
 
