b64c90ee503a03535519cd4a9d86faefa7baf5d5
Georgi Gotev <118106429+GeorgiGotev@users.noreply.github.com>
2023-05-07 11:19:02 +0300

    dom event exercise solve

--- JS courses/Advanced/JS Advanced/5-1 Exercise Dom manipulations and events/05. JS-Advanced-DOM-Events-Exercise-Resources/02. Time-Converter/solution.js
+++ JS courses/Advanced/JS Advanced/5-1 Exercise Dom manipulations and events/05. JS-Advanced-DOM-Events-Exercise-Resources/02. Time-Converter/solution.js
@@ -1,4 +1,35 @@
 function attachEventsListeners() {
 
-    console.log('TODO:...');
+    document.querySelector('body').addEventListener('click', calculate);
+    let days = document.getElementById('days');
+    let hours = document.getElementById('hours');
+    let minutes = document.getElementById('minutes');
+    let seconds = document.getElementById('seconds');
+
+    let daysButton = document.querySelector('#daysBtn');
+    let hoursButton = document.querySelector('#hoursBtn');
+    let minutesButton = document.querySelector('#minutesBtn');
+    let secondsButton = document.querySelector('#secondsBtn');
+
+    function calculate(e) {
+        let parent = e.target;
+
+        if (parent === daysButton) {
+            hours.value = days.value * 24;
+            minutes.value = days.value * 1440;
+            seconds.value = days.value * 86400;
+        } else if (parent === hoursButton) {
+           days.value=hours.value/24;
+           minutes.value=hours.value * 60;
+           seconds.value=hours.value * 3600;
+        } else if (parent === minutesButton) {
+            hours.value = minutes.value / 60;
+            days.value = minutes.value / 1440;
+            seconds.value = minutes.value *60;
+        } else if (parent === secondsButton) {
+            hours.value = seconds.value /3600;
+            minutes.value = seconds.value / 60;
+            days.value = seconds.value / 86400;
+        }
+    }
 }
