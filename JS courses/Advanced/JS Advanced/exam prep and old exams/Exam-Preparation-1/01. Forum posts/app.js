7e90e39385b610246eb1efebc53bf827116442e8
Georgi Gotev <118106429+GeorgiGotev@users.noreply.github.com>
2023-05-22 00:52:28 +0300

    modify my first exam prep for advanced

--- JS courses/Advanced/JS Advanced/exam prep and old exams/Exam-Preparation-1/01. Forum posts/app.js
+++ JS courses/Advanced/JS Advanced/exam prep and old exams/Exam-Preparation-1/01. Forum posts/app.js
@@ -2,29 +2,26 @@ window.addEventListener("load", solve);
 
 function solve() {
 
+  publishButton.addEventListener('click', publish);
   //'add new post' elements
   let titleInputElement = document.getElementById('post-title');
   let categoryInputElement = document.getElementById('post-category');
   let commentInputElement = document.getElementById('post-content');
   let publishButton = document.getElementById('publish-btn');
   //'post for review' elements;
-  let reviewItemsElement = document.getElementById('review-list');
 
+  let reviewItemsElement = document.getElementById('review-list');
 
   //upload posts elements
   let publishedElements = document.getElementById('published-list');
   let clearButton = document.getElementById('clear-btn');
 
-
-
-  publishButton.addEventListener('click', publish);
   function publish() {
     if (titleInputElement.value && categoryInputElement.value && commentInputElement.value) {
-
-
       let liElement = document.createElement('li');
       liElement.setAttribute("class", "rpost");
       reviewItemsElement.appendChild(liElement);
+
       let articleElement = document.createElement('article');
       let h4Element = document.createElement('h4');
       let firstPElement = document.createElement('p');
@@ -33,6 +30,7 @@ function solve() {
       articleElement.appendChild(firstPElement);
       articleElement.appendChild(secondPElement);
       liElement.appendChild(articleElement);
+
       let buttonEdit = document.createElement('button');
       buttonEdit.textContent = 'Edit';
       buttonEdit.setAttribute("class", "action-btn edit");
@@ -41,14 +39,6 @@ function solve() {
       buttonApprove.setAttribute("class", "action-btn approve");
       liElement.appendChild(buttonApprove);
       liElement.appendChild(buttonEdit);
-
-
-
-
-
-
-
-
 
       h4Element.textContent = titleInputElement.value;
       firstPElement.textContent = `Category: ${categoryInputElement.value}`;
@@ -78,8 +68,6 @@ function solve() {
         buttonApprove.remove()
         buttonEdit.remove()
       }
-
-
 
       clearButton.addEventListener('click', clear);
       function clear() {
--- JS courses/Advanced/JS Advanced/exam prep and old exams/Exam-Preparation-1/02. Garden/Garden.js
+++ JS courses/Advanced/JS Advanced/exam prep and old exams/Exam-Preparation-1/02. Garden/Garden.js
@@ -8,7 +8,7 @@ class Garden {
         if (this.spaceAvailable < spaceRequired) {
             throw new Error('Not enough space in the garden.')
         }
-        this.plants.push({ plantName, spaceRequired, ripe: 'false', quantity: 0 });
+        this.plants.push({ plantName, spaceRequired, ripe: false, quantity: 0 });
         this.spaceAvailable -= spaceRequired;
         return `The ${plantName} has been successfully planted in the garden.`
     }
@@ -18,30 +18,27 @@ class Garden {
         if (!this.plants.some(x => x.plantName === plantName)) {
             throw new Error(`There is no ${plantName} in the garden.`)
         }
-        if (currPlant.ripe === 'true') {
+        if (currPlant.ripe === true) {
             throw new Error(`The ${plantName} is already ripe.`);
         }
         if (quantity <= 0) {
             throw new Error('The quantity cannot be zero or negative.');
         }
-        currPlant.ripe = 'true';
+        currPlant.ripe = true;
         currPlant.quantity = quantity;
 
-
-        if (quantity === 1) {
-            return `${quantity} ${plantName} has successfully ripened.`
-        } else {
-            return `${quantity} ${plantName}s have successfully ripened.`
-        }
+        let returnText = quantity === 1 ? `${quantity} ${plantName} has successfully ripened.` :
+            `${quantity} ${plantName}s have successfully ripened.`;
+        return returnText;
 
     }
     harvestPlant(plantName) {
         let currPlant = this.plants.find(x => x.plantName === plantName);
-        
+
         if (!this.plants.some(x => x.plantName === plantName)) {
             throw new Error(`There is no ${plantName} in the garden.`)
         }
-        if (currPlant.ripe === 'false') {
+        if (currPlant.ripe === false) {
             throw new Error(`The ${plantName} cannot be harvested before it is ripe.`);
         }
 
@@ -51,6 +48,8 @@ class Garden {
 
         //delete this plant from the array 'plants';
         this.plants.splice(this.plants.findIndex(a => a.plantName === plantName), 1)
+        //other options to filter all plants which are different from this plantName;
+        // this.plants=this.plants.filter(x=>x.plantName!==plantName); 
         return `The ${plantName} has been successfully harvested.`
     }
 
@@ -58,26 +57,42 @@ class Garden {
         let result = ''
         result += `The garden has ${this.spaceAvailable} free space left.\n`;
 
-        let plantsInGarden = [];
-        this.plants.forEach(element => {
-            plantsInGarden.push(Object.entries(element)[0][1])
-        });
-        let sorted=plantsInGarden.sort((a,b)=>a.localeCompare(b));
-        result += `Plants in the garden: ${sorted.join(', ')}\n`;
+        // let plantsInGarden = [];
+        // this.plants.forEach(element => {
+        //     plantsInGarden.push(Object.entries(element)[0][1])
+        // });
+        // let sorted = plantsInGarden.sort((a, b) => a.localeCompare(b));
+        // result += `Plants in the garden: ${sorted.join(', ')}\n`;
+        result += `Plants in the garden: `
+        this.plants.sort((a, b) => a.plantName.localeCompare(b.plantName)).forEach(line => result += `${line.plantName}, `);
+        result = result.substring(0, result.length - 2)
+        result += `\n`;
 
-        if(this.storage.length>=0){
-        let plantsInStorage = '';
-        this.storage.forEach((element, i) => {
-            if (this.storage.length-1 !== i) {
-                plantsInStorage += `${Object.values(element)[0]} (${Object.values(element)[1]}), `;
-            } else {
-                plantsInStorage += `${Object.values(element)[0]} (${Object.values(element)[1]})`
-            }
-        });
-        result += `Plants in storage: ${plantsInStorage}`
-    }else{
-        result+='Plants in storage: The storage is empty.';
-    }
+
+        // if (!this.storage.length) {   //this.storage.length!==0
+        //     let plantsInStorage = '';
+        //     this.storage.forEach((element, i) => {
+        //         if (this.storage.length - 1 !== i) {
+        //             plantsInStorage += `${Object.values(element)[0]} (${Object.values(element)[1]}), `;
+        //         } else {
+        //             plantsInStorage += `${Object.values(element)[0]} (${Object.values(element)[1]})`
+        //         }
+        //     });
+        //     result += `Plants in storage: ${plantsInStorage}`
+        // } else {
+        //     result += 'Plants in storage: The storage is empty.';
+        // }
+
+        //
+        let tempArr=[];
+        result += `Plants in storage: `
+        if (!this.storage.length) {
+            result += 'Plants in storage: The storage is empty.';
+        } else {
+            this.storage.forEach(plant => tempArr.push(`${plant.plantName} (${plant.quantity})`))
+        }
+        result +=tempArr.join(', ');
+
         return result
     }
 }
@@ -86,10 +101,8 @@ class Garden {
 
 const myGarden = new Garden(250)
 console.log(myGarden.addPlant('apple', 20));
-console.log(myGarden.addPlant('orange', 200));
-console.log(myGarden.addPlant('raspberry', 10));
+console.log(myGarden.addPlant('orange', 100));
+console.log(myGarden.addPlant('cucumber', 30));
 console.log(myGarden.ripenPlant('apple', 10));
 console.log(myGarden.ripenPlant('orange', 1));
-console.log(myGarden.harvestPlant('orange'));
-console.log(myGarden.harvestPlant('apple'));
-console.log(myGarden.generateReport());
+console.log(myGarden.ripenPlant('cucumber', -5));
