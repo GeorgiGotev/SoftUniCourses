1d5e9a49c07d86b188ff9eb269876c899126c434
Georgi Gotev <118106429+GeorgiGotev@users.noreply.github.com>
2023-05-26 23:46:21 +0300

    js advanced solves

--- JS courses/Advanced/JS Advanced/6-exercise Advanced Functions/06. JS-Advanced-Advanced-Functions-Exercise-Resources/07. Simple Calculator/app.js
+++ JS courses/Advanced/JS Advanced/6-exercise Advanced Functions/06. JS-Advanced-Advanced-Functions-Exercise-Resources/07. Simple Calculator/app.js
@@ -1,7 +1,43 @@
 function calculator() {
-    // TODO:
+    let firstEl = '';
+    let secondEl = '';
+    let resultEl = '';
+    let result = {
+        init(a, b, c) {
+            firstEl = document.querySelector(a);
+            secondEl = document.querySelector(b);
+            resultEl = document.querySelector(c);
+        },
+        add() {
+            resultEl.value = Number(firstEl.value) + Number(secondEl.value);
+        },
+        subtract() {
+            resultEl.value = Number(firstEl.value) - Number(secondEl.value);
+        },
+    };
+    return result;
 }
+const calculate = calculator();
+console.log(calculate.init('#num1', '#num2', '#result'));
 
+// function calculator() {
+//     const html = { s1: "", s2: "", output: "" }
 
+//     function calc(a, b, sign) {
+//         const signs = { "+": (a, b) => a + b, "-": (a, b) => a - b }
 
+//         return signs[sign](Number(a), Number(b))
+//     }
 
+//     return {
+//         init: (a, b, c) => {
+//             html.s1 = document.querySelector(a)
+//             html.s2 = document.querySelector(b)
+//             html.output = document.querySelector(c)
+//         },
+//         add: () =>
+//             (html.output.value = calc(html.s1.value, html.s2.value, "+")),
+//         subtract: () =>
+//             (html.output.value = calc(html.s1.value, html.s2.value, "-")),
+//     }
+// }
--- JS courses/Advanced/JS Advanced/6-exercise Advanced Functions/06. JS-Advanced-Advanced-Functions-Exercise-Resources/08. Next Article/script.js
+++ JS courses/Advanced/JS Advanced/6-exercise Advanced Functions/06. JS-Advanced-Advanced-Functions-Exercise-Resources/08. Next Article/script.js
@@ -1,3 +1,17 @@
 function getArticleGenerator(articles) {
-    // TODO
+    let copyArr = articles.slice();
+    let result = document.getElementById('content');
+
+    function showMore() {
+        if (copyArr.length > 0) {
+            let currText = copyArr.shift();
+            let article=document.createElement('article');
+            article.textContent=currText;
+            result.appendChild(article);
+        }
+    }
+    return showMore;
 }
+
+
+
