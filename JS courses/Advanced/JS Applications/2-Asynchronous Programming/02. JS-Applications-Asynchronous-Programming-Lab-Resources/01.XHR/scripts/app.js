afe78a236c7768736a986af9a5c1702cda1f937c
Georgi Gotev <118106429+GeorgiGotev@users.noreply.github.com>
2023-06-21 00:47:31 +0300

    js app start!

--- JS courses/Advanced/JS Applications/2-Asynchronous Programming/02. JS-Applications-Asynchronous-Programming-Lab-Resources/01.XHR/scripts/app.js
+++ JS courses/Advanced/JS Applications/2-Asynchronous Programming/02. JS-Applications-Asynchronous-Programming-Lab-Resources/01.XHR/scripts/app.js
@@ -1,3 +1,25 @@
+let button=document.querySelector('button');
+
 function loadRepos() {
-   console.log("TODO...");
+    // e.preventDefault()
+
+    let result = document.getElementById('res');
+    let url = `https://api.github.com/users/testnakov/repos`;
+    let req = new XMLHttpRequest();
+
+    req.addEventListener('readystatechange', () => {
+        if (req.readyState === 4 && req.status === 200) {
+            result.textContent = req.responseText;
+        }
+    });
+    if(button.textContent==='Load Repos'){
+      button.textContent='Hide Repos';
+      result.style.display='block'
+    }else{
+      button.textContent='Load Repos';
+      result.style.display='none';
+    }
+
+    req.open('GET', url);
+    req.send();
 }
--- JS courses/Advanced/JS Applications/2-Asynchronous Programming/02. JS-Applications-Asynchronous-Programming-Lab-Resources/02.Github-Repos/scripts/app.js
+++ JS courses/Advanced/JS Applications/2-Asynchronous Programming/02. JS-Applications-Asynchronous-Programming-Lab-Resources/02.Github-Repos/scripts/app.js
@@ -1,3 +1,51 @@
 function loadRepos() {
-	console.log("TODO...");
+    let usrName = document.getElementById('username');
+    let url = `https://api.github.com/users/${usrName.value}/repos`;
+    let reposField = document.getElementById('repos');
+
+    async function getPersonRepo() {
+    	try{
+    		reposField.replaceChildren()
+
+    		let res = await fetch(url);
+    		let data = await res.json();
+
+    		data.forEach((element) => {
+    			let li = document.createElement('li');
+    			let a = document.createElement('a');
+    			a.textContent = element.full_name;
+    			a.href = element.html_url;
+
+    			li.appendChild(a);
+    			reposField.appendChild(li);
+    		});
+    	}catch(err){
+    		err='Not Found!'
+    		alert(err);
+    		reposField.replaceChildren()
+    	}
+    }
+	// reposField.replaceChildren()
+    // fetch(url)
+    //     .then((res) => res.json())
+    //     .then((data) => {
+    //         data.map((element) => {
+    //             let li = document.createElement('li');
+    //             let a = document.createElement('a');
+    //             a.textContent = element.full_name;
+    //             a.href = element.html_url;
+
+    //             li.appendChild(a);
+    //             reposField.appendChild(li);
+    //         });
+    //     })
+	// 	.catch((err)=>{
+	// 		err='Not Found';
+	// 		alert(err);
+	// 		reposField.replaceChild();
+	// 	})
+		
+
+    usrName.value = '';
+    getPersonRepo();
 }
