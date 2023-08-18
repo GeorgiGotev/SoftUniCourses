a56c815c2c1d322c31930423b4dc766f0a1b2ee1
Georgi Gotev <118106429+GeorgiGotev@users.noreply.github.com>
2023-08-18 14:07:57 +0300

    home page and static to cubicle

--- JS courses/JS Back-End/4-Workshop- ExpressJS and Templating/demo/src/index.js
+++ JS courses/JS Back-End/4-Workshop- ExpressJS and Templating/demo/src/index.js
@@ -1,11 +1,23 @@
 const express= require ('express');
+const handlebars= require('express-handlebars');
+const path=require('path')
 
 const app=express();
 
+
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+}))
+app.set('view engine', 'hbs');
+app.set('views', 'src/views');
+
+
 const PORT=3000;
 
+app.use(express.static('./src/public'))
+
 app.get('/', (req,res)=>{
-    res.send('hello')
+    res.render('index')
 })
 
 app.listen(PORT, ()=> console.log(`server is running on port ${PORT}...`))
