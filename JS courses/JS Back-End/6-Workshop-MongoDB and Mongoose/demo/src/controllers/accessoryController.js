5f52c5e5cdae0838bb2a8af9ed83f52cb20eb8d0
Georgi Gotev <118106429+GeorgiGotev@users.noreply.github.com>
2023-08-24 22:46:14 +0300

    add post action to create accessory

--- JS courses/JS Back-End/6-Workshop-MongoDB and Mongoose/demo/src/controllers/accessoryController.js
+++ JS courses/JS Back-End/6-Workshop-MongoDB and Mongoose/demo/src/controllers/accessoryController.js
@@ -4,6 +4,14 @@ router.get('/create', (req,res)=>{
     res.render('accessory/create');
 })
 
-// router.post()
+router.post('/create', (req,res)=>{
+
+    const {name,description,imageUrl}=req.body;
+
+    //add to DB
+
+    res.redirect('/');
+
+})
 
 module.exports=router;
