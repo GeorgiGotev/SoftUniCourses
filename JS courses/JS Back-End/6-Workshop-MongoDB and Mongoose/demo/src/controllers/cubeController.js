184911710725cd840c3f716fd4f3c144cd11149e
Georgi Gotev <118106429+GeorgiGotev@users.noreply.github.com>
2023-08-25 23:15:54 +0300

    attach only unattached accessories

--- JS courses/JS Back-End/6-Workshop-MongoDB and Mongoose/demo/src/controllers/cubeController.js
+++ JS courses/JS Back-End/6-Workshop-MongoDB and Mongoose/demo/src/controllers/cubeController.js
@@ -16,14 +16,14 @@ router.get('/:cubeId/details', async (req, res) => {
     const id = req.params.cubeId;
     
     let currCube = await cubeService.getById(id).lean();
-    
+
     res.render('details', { currCube });
 });
 
 router.get('/:cubeId/attach-accessory', async (req, res) => {
     const cube = await cubeService.getById(req.params.cubeId).lean();
 
-    const accessories = await accessoryService.getAll().lean();
+    const accessories = await accessoryService.getLeft(cube.accessories).lean();
 
     const hasAccessories = accessories.length > 0;
 
--- JS courses/JS Back-End/6-Workshop-MongoDB and Mongoose/demo/src/service/accessoryService.js
+++ JS courses/JS Back-End/6-Workshop-MongoDB and Mongoose/demo/src/service/accessoryService.js
@@ -3,3 +3,6 @@ const Accessory=require('../models/Accessory');
 exports.getAll=()=> Accessory.find();
 
 exports.create=(accessoryData)=> Accessory.create(accessoryData);
+
+exports.getLeft=(accessoryIds)=>Accessory.find({_id:{$nin:accessoryIds}});
+// exports.getLeft=(accessoryIds)=>Accessory.find(_id).nin(accessoryIds);
