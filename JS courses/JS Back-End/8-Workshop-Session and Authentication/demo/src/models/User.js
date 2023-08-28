59213043486e348c5749bee4293909163f32d2ce
Georgi Gotev <118106429+GeorgiGotev@users.noreply.github.com>
2023-08-28 21:33:11 +0300

    has password pre save

--- JS courses/JS Back-End/8-Workshop-Session and Authentication/demo/src/models/User.js
+++ JS courses/JS Back-End/8-Workshop-Session and Authentication/demo/src/models/User.js
@@ -1,8 +1,20 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     username: String,
     password: String,
 });
+
+userSchema.virtual('repeatPassword').set(function (value) {
+    if (value !== this.password) {
+        throw new mongoose.MongooseError('Password mismatch!');
+    }
+});
+
+userSchema.pre('save',async function(){
+    const hash=await bcrypt.hash(this.password, 10);
+    this.password=hash;
+})
 
 module.exports = mongoose.model('User', userSchema);
