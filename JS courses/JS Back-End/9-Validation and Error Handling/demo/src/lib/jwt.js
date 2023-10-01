126fd1eae35778b4b54060dbb4e4c8c4c6b5a3f7
Georgi Gotev <118106429+GeorgiGotev@users.noreply.github.com>
2023-10-01 21:47:35 +0300

    cm

--- JS courses/JS Back-End/9-Validation and Error Handling/demo/src/lib/jwt.js
+++ JS courses/JS Back-End/9-Validation and Error Handling/demo/src/lib/jwt.js
@@ -1,6 +1,7 @@
 const jsonwebtoken=require('jsonwebtoken');
 const {promisify}=require('util');
 
+
 const jwt={
     sign: promisify(jsonwebtoken.sign),
     verify: promisify(jsonwebtoken.verify),
