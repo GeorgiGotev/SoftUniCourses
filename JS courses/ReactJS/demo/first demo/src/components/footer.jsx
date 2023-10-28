d3d1862fb11ddcf260f606b6799834c429638273
Georgi Gotev <118106429+GeorgiGotev@users.noreply.github.com>
2023-10-28 06:31:58 +0300

    react components

--- JS courses/ReactJS/demo/first demo/src/components/footer.jsx
+++ JS courses/ReactJS/demo/first demo/src/components/footer.jsx
@@ -1,6 +1,10 @@
 export default function Footer(props) {
     return (
         <section className="container-fluid footer_section">
+            <a href=""> <span>Политика за личните данни</span></a>
+            <br />
+            <a href=""> <span>Контакти</span></a>
+
             <p>
                 Copyright © 2023 All Rights Reserved By
                 <a href="https://html.design/"> Georgi Gotev</a>
