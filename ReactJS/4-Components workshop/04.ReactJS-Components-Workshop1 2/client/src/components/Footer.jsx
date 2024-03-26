e7cac9fc230af6f266374a775f76e2c8fe6543c7
Georgi Gotev <118106429+GeorgiGotev@users.noreply.github.com>
2024-03-26 11:08:05 +0200

    das

---
+++ ReactJS/4-Components workshop/04.ReactJS-Components-Workshop1 2/client/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <p>This site is designed to be used for training purposes at SoftUni.</p>
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ ReactJS/4-Components workshop/04.ReactJS-Components-Workshop1 2/client/src/components/UserList.jsx
@@ -0,0 +1,14 @@
+import Search from "./Search";
+import UserListTable from "./UserListTable";
+
+const UserList = () => {
+    return (
+        <section className="card users-container">
+            <Search />
+            
+            <UserListTable />
+        </section>
+    );
+};
+
+export default UserList;
