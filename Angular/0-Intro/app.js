53224ce35f13716ab973be34fb34fde836ed7d71
Georgi Gotev <g.gotev@abv.bg>
2025-04-18 23:20:47 +0300

    angular intro lab

---
+++ Angular/0-Intro/app.js
@@ -0,0 +1,4 @@
+var a = 1;
+console.log(a);
+var numberArray = [1, 2, 3, 4];
+console.log(numberArray);
---
+++ Angular/0-Intro/app.ts
@@ -0,0 +1,5 @@
+const a: number = 1;
+console.log(a);
+
+const numberArray: number[] = [1, 2, 3, 4];
+console.log(numberArray);
